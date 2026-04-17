ca3f6f31aa081660f3b30cfd986a99f145527d03
Matheus Benatti <matheus.cwb2@gmail.com>
2026-04-16 23:21:07 -0300

    refactor: added table schema

--- src/ingestion/load_data.py
+++ src/ingestion/load_data.py
@@ -7,10 +7,36 @@ def read_and_insert_data_from_csv(conn, file_path):
     logger.info(f"[INGESTION] Starting load: {file_path}")
 
     try:
-        with open(file_path, "r", encoding="utf-8") as f:
+        with open(file_path, "r", encoding="utf-8", errors="replace") as f:
             cur.copy_expert(
                 """
-                COPY expedia_raw
+                COPY expedia_raw (
+                    id,
+                    date_time,
+                    site_name,
+                    posa_continent,
+                    user_location_country,
+                    user_location_region,
+                    user_location_city,
+                    orig_destination_distance,
+                    user_id,
+                    is_mobile,
+                    is_package,
+                    channel,
+                    srch_ci,
+                    srch_co,
+                    srch_adults_cnt,
+                    srch_children_cnt,
+                    srch_rm_cnt,
+                    srch_destination_id,
+                    srch_destination_type_id,
+                    is_booking,
+                    cnt,
+                    hotel_continent,
+                    hotel_country,
+                    hotel_market,
+                    hotel_cluster
+                )
                 FROM STDIN
                 WITH CSV HEADER
                 """,
